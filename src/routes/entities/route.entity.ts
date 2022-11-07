b88e5d21274b613bd1202550a1324b80aa7c5697
pedro.carvalho <pedro.carvalho@ifood.com.br>
2022-11-07 01:21:53 -0300

    created persistense data and send kafka msg

---
+++ src/routes/entities/route.entity.ts
@@ -0,0 +1,26 @@
+import { Schema, Prop, raw, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type RouterDcoument = Route & Document;
+
+@Schema()
+export class Route {
+  @Prop()
+  title: string;
+  @Prop(
+    raw({
+      lat: { type: Number },
+      lng: { type: Number },
+    }),
+  )
+  startPosition: { lat: number; lng: number };
+  @Prop(
+    raw({
+      lat: { type: Number },
+      lng: { type: Number },
+    }),
+  )
+  endPosition: { lat: number; lng: number };
+}
+
+export const RouteSchema = SchemaFactory.createForClass(Route);
